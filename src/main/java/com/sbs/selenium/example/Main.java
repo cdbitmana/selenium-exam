655366850ab6faefa716ce26c1d8fb2ed358bbcd
cdbitmana <cdbitmana@gmail.com>
2020-12-01 12:58:27 +0900

    자바로 이미지 크롤링

--- src/main/java/com/sbs/selenium/example/Main.java
+++ src/main/java/com/sbs/selenium/example/Main.java
@@ -1,10 +1,18 @@
 package com.sbs.selenium.example;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -26,13 +34,46 @@ public class Main {
 		// WebDriver 객체 생성
 		ChromeDriver driver = new ChromeDriver(options);
 
-		
-		
 		List<String> tabs = new ArrayList<String>(driver.getWindowHandles());
-		
+
 		driver.switchTo().window(tabs.get(0));
-		driver.get("https://www.naver.com");
+		driver.get("https://unsplash.com/t/nature");
+
+		File downloadsFolder = new File("downloads");
+
+		if (downloadsFolder.exists() == false) {
+			downloadsFolder.mkdir();
+		}
+
+		Util.sleep(1000);
+
+		List<WebElement> imgElements = driver.findElements(
+				By.cssSelector("[data-test=\"masonry-grid-count-three\"] img[data-test=\"photo-grid-multi-col-img\"]"));
+
+		for (WebElement imgElement : imgElements) {
+			String src = imgElement.getAttribute("src");
+
+			BufferedImage saveImage = null;
+
+			try {
+				saveImage = ImageIO.read(new URL(src));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			if (saveImage != null) {
+				try {
+
+					String fileName = src.split("/")[3];
+					fileName = fileName.split("\\?")[0];
+					ImageIO.write(saveImage, "jpg", new File("downloads/" + fileName + ".jpg"));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			System.out.println(src);
+		}
 
 	}
-
 }
